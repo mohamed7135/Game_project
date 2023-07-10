26f2fdb1ea53e145a3dd572f689e330999eaea10
Rane-19 <dia1929@yahoo.com.ph>
2023-07-10 11:31:21 -0700

    changes

--- script/script.js
+++ script/script.js
@@ -1,104 +1,82 @@
 'use strict';
-
 document.addEventListener('DOMContentLoaded', () => {
     const levels = {
         easy: 6,
         medium: 8,
         hard: 12
     };
-
     const cardArray = [
         {
             name:'bulbausar',
             img: 'IMages/bulbausar1.png'
         },
-
         {
             name:'bulbausar',
             img: 'IMages/bulbausar1.png'
         },
-
         {
             name:'charmander',
             img: 'IMages/charmander1.png'
         },
-
         {
             name:'charmander',
             img: 'IMages/charmander1.png'
         },
-
-
         {
             name:'mudkip',
             img: 'IMages/mudkip1.png'
         },
-
         {
             name:'mudkip',
             img: 'IMages/mudkip1.png'
         },
-
         {
             name:'pikachu',
             img: 'IMages/pikachu1.png'
         },
-
         {
             name:'pikachu',
             img: 'IMages/pikachu1.png'
         },
-        
         {
             name:'squirtle',
             img: 'IMages/squirtle1.png'
         },
-
         {
             name:'squirtle',
             img: 'IMages/squirtle1.png'
         },
-
         {
             name:'torchic',
             img: 'IMages/torchic1.png'
         },
-
         {
             name:'torchic',
             img: 'IMages/torchic1.png'
         },
-
         {
             name:'totodile',
             img: 'IMages/totodile1.png'
         },
-
         {
             name:'totodile',
             img: 'IMages/totodile1.png'
         },
-
         {
             name:'trecko',
             img: 'IMages/trecko1.png'
         },
-
         {
             name:'trecko',
             img: 'IMages/trecko1.png'
         },
     ]
-
-    cardArray.sort(() => 0.5 - Math.random())
-
-    const grid = document.querySelector('.grid')
-    const resultDisplay = document.querySelector('#result')
-    var cardsChosen = []
-    var cardsChosenId = []
-    var cardsWon = []
-
-  
+    cardArray.sort(() => 0.5 - Math.random());
+    const grid = document.querySelector('.grid');
+    const resultDisplay = document.querySelector('#result');
+    let cardsChosen = [];
+    let cardsChosenId = [];
+    let cardsWon = [];
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             var card = document.createElement('img')
@@ -108,8 +86,6 @@ document.addEventListener('DOMContentLoaded', () => {
             grid.appendChild(card)
         }
     }
-
-    
      function checkForMatch() {
         var cards = document.querySelectorAll('img')
         const optionOneId = cardsChosenId[0]
@@ -128,12 +104,10 @@ document.addEventListener('DOMContentLoaded', () => {
         cardsChosenId = []
         resultDisplay.textContent = cardsWon.length
         if (cardsWon.length === cardArray.length/2) {
-            resultDisplay.textContent = 'Congrats!'
+            alert('You Won!');;
+            displayGameOverScreen();
         }
         }
-     
-
-   
     function flipCard() {
         var cardId = this.getAttribute('data-id');
         cardsChosen.push(cardArray[cardId].name);
@@ -142,9 +116,36 @@ document.addEventListener('DOMContentLoaded', () => {
         if (cardsChosen.length === 2) {
           setTimeout(checkForMatch, 500);
         }
-      }    
-      
+      }
+      $('#start-btn').click(() => displayGameScreen() );
+      $('#quit-btn').click(() => displayGameOverScreen() );
+      $('#home-btn').click(() => displaywelcomescreen() );
+      $('#play-again-btn').click(() => displayGameScreen() );
+      $('#info-btn').click(() => instructionsscreen() );
+      $('#home-btn-2').click(() => displaywelcomescreen() );
       createBoard()
-      
       })
-      
+      function displayGameScreen() {
+        $("#welcome-screen").addClass("hidden");
+        $("#game-screen").removeClass("hidden");
+        $("#game-over-screen").addClass("hidden");
+        $("#instructions-screen").addClass("hidden");
+    }
+    function displayGameOverScreen() {
+        $("#welcome-screen").addClass("hidden");
+        $("#game-screen").addClass("hidden");
+        $("#game-over-screen").removeClass("hidden");
+        $("#instructions-screen").addClass("hidden");
+    }
+    function displaywelcomescreen() {
+        $("#welcome-screen").removeClass("hidden");
+        $("#game-screen").addClass("hidden");
+        $("#game-over-screen").addClass("hidden");
+        $("#instructions-screen").addClass("hidden");
+    }
+    function instructionsscreen() {
+        $("#welcome-screen").addClass("hidden");
+        $("#game-screen").addClass("hidden");
+        $("#game-over-screen").addClass("hidden");
+        $("#instructions-screen").removeClass("hidden");
+    }
